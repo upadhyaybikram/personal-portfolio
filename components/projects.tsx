646c70a4bbb804cd357ef65c06ee969d9b05b73f
Bikram Upadhyay <upadhyaybikram@gmail.com>
2024-01-29 14:23:53 +1100

    #Link tag added to proejct component #data updated

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,18 +5,30 @@ import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
+import Link from "next/link";
 
 export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
 
-  return (
+    function getProjectUrl(project) {
+        const title = project.title.toLowerCase();
+        if(title.includes('admin')){
+            return "https://property.credit/";
+        } else {
+            return "https://auctionslive.com/";
+        }
+    }
+
+    return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
-          <React.Fragment key={index}>
-            <Project {...project} />
-          </React.Fragment>
+            <Link key={index} href={getProjectUrl(project)} passHref target="_blank">
+                    <React.Fragment key={index}>
+                        <Project {...project} />
+                    </React.Fragment>
+            </Link>
         ))}
       </div>
     </section>
